16b6e0af1631c325cb46df1e46fe8464fe27de13
Ryan Gahart <ryan.gahart@gmail.com>
2015-07-11 16:06:17 -0500

    add CarController.cs

---
+++ hackathon/Assets/Assets/Scripts/CarController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	public float power;
+	public float turnPower;
+	private Rigidbody2D rigidbody2D;
+	
+	void Start() {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+
+		float accel = Input.GetAxis ("Vertical");
+		float steer = Input.GetAxis ("Horizontal");
+
+		float rot = transform.localEulerAngles.z - steer * turnPower;
+		transform.localEulerAngles = new Vector3 (0, 0, rot);
+
+//		rigidbody2D.AddTorque (steer * turnPower);
+		rigidbody2D.AddForce (transform.up * -accel * power);
+
+
+
+		killOrthogonalVelocity ();
+	}
+
+	void killOrthogonalVelocity() {
+		Vector2 forwardVelocity = -transform.forward * Vector2.Dot (rigidbody2D.velocity, transform.forward);
+		Vector2 rightVelocity = -transform.right * Vector2.Dot (rigidbody2D.velocity, transform.right);
+		rigidbody2D.velocity = forwardVelocity + rightVelocity * 0.8f;
+	}
+}
